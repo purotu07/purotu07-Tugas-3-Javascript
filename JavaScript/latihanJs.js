4265d0a4773ada3f99fadce04eb7dd9d05c46945
purotu07 <145517853+purotu07@users.noreply.github.com>
2023-12-13 22:38:06 +0700

    Add files via upload

---
+++ JavaScript/latihanJs.js
@@ -0,0 +1,11 @@
+var newSiswa = "Puthut"
+let usia = 16
+let beratBadan = 72
+let tinggiBadan = 170
+let status =prompt('Masukkan Nama Anda: ')
+
+console.log('nama peserta: ' + newSiswa + ' usia anda saat ini adalah ' + usia +
+    ' tinggi badan saya ' + tinggiBadan + ' berat badan saya ' + beratBadan + ' status siswa')
+
+alert('nama peserta: ' + newSiswa + ' usia anda saat ini adalah ' + usia +
+    ' tinggi badan saya ' + tinggiBadan + ' berat badan saya ' + beratBadan + ' status siswa')
